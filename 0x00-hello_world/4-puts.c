846f3e4182cd1789ee9705e82654eec53f111f1d
Rodah Chep <chepkorirrodah36@gmail.com>
2022-10-20 05:18:14 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
+
 /**
  * mai - Entry point
  *
  * Return: Always 0 (success)
  */
-
 int main(void)
 {
-	puts("\"Programing is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return(0);
 }
